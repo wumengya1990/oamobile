54d65f76ca1370fee5f0deda1276f9c0d056d4b8
wumengya1990 <wumengya@aliyun.com>
2019-08-13 16:38:39 +0800

    添加控制内容

--- src/components/menuAll.vue
+++ src/components/menuAll.vue
@@ -27,6 +27,11 @@
     <em><img :src="img3"></em>
     <span>我的请假申请</span>
     </router-link>
+    <van-dropdown-menu>
+    <van-dropdown-item v-model="value1" :options="option1" />
+    <van-dropdown-item v-model="value2" :options="option2" />
+    </van-dropdown-menu>
+
 
     
     </div>
@@ -44,7 +49,19 @@ export default {
                 leaveTotal:0,
                 noticeTotal:0,
                 officeTotal:0
-            }
+            },
+            value1: 0,
+            value2: 'a',
+            option1: [
+                { text: '全部商品', value: 0 },
+                { text: '新款商品', value: 1 },
+                { text: '活动商品', value: 2 }
+            ],
+            option2: [
+                { text: '默认排序', value: 'a' },
+                { text: '好评排序', value: 'b' },
+                { text: '销量排序', value: 'c' },
+            ]
         }
     },
     beforeCreate() {
